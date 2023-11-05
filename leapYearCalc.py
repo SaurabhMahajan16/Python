3d1de7bcd9240057187e81b17e97cdfe5db24ad4
Saurabh Mahajan <srbhmahajan61@live.com>
2023-11-04 23:20:17 -0400

    photofare

---
+++ leapYearCalc.py
@@ -0,0 +1,8 @@
+# Which year do you want to check?
+year = int(input())
+# 🚨 Don't change the code above 👆
+if year%4 == 0:
+  print("Leap year")
+else:
+  print("Not leap year")
+# Write your code below this line 👇
--- ticketRollerCoaster.py
+++ ticketRollerCoaster.py
@@ -1,24 +1,34 @@
 print("welcome to roller coaster ticketing booth")
+adultfare=20
+childrenfare=15
+seniorfare=13
+photofare=3
 height=int(input("what is your height in cm "))
 if height <= 120:
     print("you are not allowed to ride the roller coaster")
 else:
     print("you are allowed to ride the roller coaster")
+    phototaken=input("do you want photo to be taken? \n the price of photo is 3$ each \n Y or N \n")
     totalTicketsRequired=int(input("how many total tickets you want? "))
     if totalTicketsRequired == 1:
         age=int(input("what is your age? "))
-        if age>=18:
-            print(f"price of one ticket for adult is 20$")
+        if age>=18 and phototaken == "Y":
+            #print(f"price of one ticket for adult is 20$")
+            print(f"your total is {photofare+adultfare}$")
+        elif age>=18 and phototaken == "N":
+            print(f"your total is {adultfare}$")
+        elif age < 18 and phototaken == "Y":
+            print(f"your total is {photofare+childrenfare}$")
+        elif age<18 and phototaken == "N":
+            print(f"your total is {childrenfare}$")
             
+        elif age>=60 and phototaken == "Y":
+            print(f"your total is {photofare+seniorfare}$")
+        elif age>=18 and phototaken == "N":
+            print(f"your total is {seniorfare}$")
             
-        elif age < 18:
-            print(f"price of one ticket for children is 15$")
-            
-        elif age>=60:
-            print(f"price of one ticket for senior is 13")
-            
-        #ticketsRequired=int(input("how many adult tickets you want? "))
-    elif totalTicketsRequired > 1:
+        
+    elif totalTicketsRequired > 1 and phototaken == "Y":
         print(f"price of one ticket for adult is 20 \n price of one ticket for children is 15 \n price of one ticket for senior is 13")
         childrenTickets=int(input("do you want any children tickets, \n if yes then how many would you want \n the age of children has to be less than 12 ? "))
         seniorTickets=int(input("do  you want any senior tickets? \n if yes then how many would you want? \n the age of senior has to be greater than 60? "))
@@ -30,5 +40,18 @@ else:
         #seniorTickets=int(input("how many senior tickets? "))
         #childrenTickets=int(input("how many children tickets? "))
         #totalPriceOfTickets=20*adultTickets+15*childrenTickets+13*seniorTickets
-        print(f"your total is {20*adultTickets+15*childrenTickets+13*seniorTickets}$")
+        print(f"your total is {adultfare*adultTickets+childrenfare*childrenTickets+seniorfare*seniorTickets+totalTicketsRequired*photofare}$")
+    elif totalTicketsRequired > 1 and phototaken == "N":
+        print(f"price of one ticket for adult is 20 \n price of one ticket for children is 15 \n price of one ticket for senior is 13")
+        childrenTickets=int(input("do you want any children tickets, \n if yes then how many would you want \n the age of children has to be less than 12 ? "))
+        seniorTickets=int(input("do  you want any senior tickets? \n if yes then how many would you want? \n the age of senior has to be greater than 60? "))
+        adultTickets=int(input("do  you want any adult tickets? \n if yes then how many would you want? \n the age of adult is 18 or greater "))
+
+        #print(f"price of one ticket for adult is 20 \n price of one ticket for children is 15 \n price of one ticket for senior is 13")
+
+        #adultTickets=int(input("how many adult tickets? "))
+        #seniorTickets=int(input("how many senior tickets? "))
+        #childrenTickets=int(input("how many children tickets? "))
+        #totalPriceOfTickets=20*adultTickets+15*childrenTickets+13*seniorTickets
+        print(f"your total is {adultfare*adultTickets+childrenfare*childrenTickets+seniorfare*seniorTickets}$")
     
